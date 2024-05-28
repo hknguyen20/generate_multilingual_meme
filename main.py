fa122874566317dc9c77e3a9e854ec5b07db641f
hknguyen20 <20nguyen.hk@vinuni.edu.vn>
2024-05-28 19:42:22 +0000

    Merged clean_text with enhance_image into main.py

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import numpy as np
 import easyocr
 import os
 import argparse
+from PIL import Image, ImageEnhance
 
-reader = easyocr.Reader(['en'])
+################### TEXT REMOVE ##########################
 
 def get_meme_text(image):
     coordinates = []
@@ -100,21 +101,66 @@ def process_image(image_path, cleaned_dir):
 
     print('Done inpainting', image_path)
 
+################### ENHANCE IMAGE ##########################
+
+def enhance_image(image_path, enhanced_dir, color=1., contrast=1., sharpness=1., brightness=1.):
+    """
+    Slightly enhance the image by adjusting its contrast, sharpness, and brightness.
+    Parameters:
+        image_path (str): Path to the input image.
+        output_path (str): Path to save the enhanced image.
+        color/contrast/sharpness/brightness (float): Factor by which to enhance the respective. Default to unchanged (1.0).
+            - color: 0.0 gives black and white, >1.0 gives higher color saturation
+            - contrast: 0.0 gives solid gray, >1.0 gives higher contrast
+            - brightness: 0.0 gives black, >1.0 gives higher brightness
+            - sharpness: 0.0 gives blurred, >1.0 gives more sharpened image
+    """
+    image = Image.open(image_path)
+    
+    enhancer = ImageEnhance.Color(image)
+    image = enhancer.enhance(color)
+    
+    enhancer = ImageEnhance.Contrast(image)
+    image = enhancer.enhance(contrast)
+    
+    enhancer = ImageEnhance.Sharpness(image)
+    image = enhancer.enhance(sharpness)
+    
+    enhancer = ImageEnhance.Brightness(image)
+    image = enhancer.enhance(brightness)
+    
+    os.makedirs(enhanced_dir, exist_ok=True)
+    image.save(f"{enhanced_dir}/{os.path.basename(image_path)}")
+
+
 if __name__ == "__main__":
     image_type = ('.png','.jpg','.jpeg')
     parser = argparse.ArgumentParser()
-    parser.add_argument('--img_dir', type=str, help='Relative path to raw image folder',required=True)
-    parser.add_argument('--cleaned_dir', type=str, help='Relative path to directory storing text-removed images',required=False, default='img_cleaned/')
+    parser.add_argument('--mode', type=str, required=True, choices=['clean','enhance'])
+    
+    parser.add_argument('--img_dir', type=str, help='Relative path to raw image folder',required=False, default='img/')
+    parser.add_argument('--cleaned_dir', type=str, help='Relative path to directory text-removed images',required=False, default='img_cleaned/')
+    parser.add_argument('--enhanced_dir', type=str, help='Relative path to enhanced image folder',required=False, default='img_enhanced/')
     
     args = parser.parse_args()
+    
     img_dir = os.path.abspath(args.img_dir)
     cleaned_dir = os.path.abspath(args.cleaned_dir)
+    enhanced_dir = os.path.abspath(args.enhanced_dir)
     # parse arguments    
-    print("Cleaning images at:", img_dir)
-    for filename in os.listdir(img_dir):
-        if filename.lower().endswith(image_type):
-            image_path = os.path.join(img_dir, filename)
-            process_image(image_path, cleaned_dir)
-    print("Cleaned and save images to:", cleaned_dir)
-
+    if args.mode=='clean':
+        reader = easyocr.Reader(['en'])    
+        print("Cleaning images at:", img_dir)
+        for filename in os.listdir(img_dir):
+            if filename.lower().endswith(image_type):
+                image_path = os.path.join(img_dir, filename)
+                process_image(image_path, cleaned_dir)
+        print("Cleaned and save images to:", cleaned_dir)
+        
+    elif args.mode=='enhance':
+        for filename in os.listdir(cleaned_dir):
+            if filename.lower().endswith(image_type):
+                image_path = os.path.join(cleaned_dir, filename)
+                enhance_image(image_path, enhanced_dir, color=1.25, contrast=1.25, sharpness=1.45, brightness=1.15)
+        print('Enhanced and saved enhanced images to:', enhanced_dir)
         
